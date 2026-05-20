09a589ddb7eb1decf77463418016a35249f0cebd
capsystemsjogoshibridos <comunicap@gmail.com>
2026-05-20 16:55:44 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
 window.EMOJI_WARS_CONFIG = {
-    // Deixe vazio para tentar conectar no mesmo domínio.
-    // Se o frontend estiver na Vercel e o server em outro host, coloque a URL aqui.
-    // Exemplo: socketServerUrl: 'https://emoji-wars-server.onrender.com'
-    socketServerUrl: ''
+    socketServerUrl: 'https://warms.onrender.com'
 };
